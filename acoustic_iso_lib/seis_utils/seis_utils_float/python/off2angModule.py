Fixed scaling in pseudo inverse for Off2Ang

acoustic_iso_lib/seis_utils/seis_utils_float/python/off2angModule.py
@@ -106,8 +106,8 @@ class off2ang2D(Op.Operator):
 			mask = (np.abs(np.outer(np.tan(g_vals),kz_axis)) <= khx_max).astype(np.int)
 
 		for g_idx,g_val in enumerate(g_vals):
-			# scale = kz_axis/(2.0*np.pi*np.cos(g_val)) if self.p_inv else 1.0
-			scale = kz_axis/(2.0*np.pi*np.cos(g_val)*np.cos(g_val)+epsilon) if self.p_inv else 1.0
+			# scale = kz_axis/(np.cos(g_val)*np.cos(g_val)) if self.p_inv else 1.0
+			scale = kz_axis/(np.cos(g_val)*np.cos(g_val)+epsilon) if self.p_inv else 1.0
 			if self.anti_alias:
 				m_tmp[g_idx,:,:] = scale*np.sum(d_kz[:,:,:]*np.exp(exp_arg*np.tan(g_val))*mask[g_idx,:],axis=0)
 			else: