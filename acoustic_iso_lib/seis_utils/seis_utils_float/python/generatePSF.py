2e8c4641213650cfa07bd4096ee3388d5881c2fa
Ettore Biondi <ettore88@stanford.edu>
2020-11-11 20:26:37 -0800

    Simplified PSF scripts

--- acoustic_iso_lib/seis_utils/seis_utils_float/python/generatePSF.py
+++ acoustic_iso_lib/seis_utils/seis_utils_float/python/generatePSF.py
@@ -36,24 +36,18 @@ if __name__ == "__main__":
 	nPSF3 = parObject.getInt("nPSF3",1)
 
 	if nPSF3 != 1 and nExt == 1:
-		raiseValueError("ERROR! nPSF3 must be 1 for non-extended images")
+		raise ValueError("ERROR! nPSF3 must be 1 for non-extended images")
 	if nPSF3 == 1 and nExt != 1:
-		raiseValueError("ERROR! nPSF3 must be different than 1 for extended images")
+		raise ValueError("ERROR! nPSF3 must be different than 1 for extended images")
 	if nPSF1 <= 0 or nPSF2 <= 0 or nPSF3 <= 0:
-		raiseValueError("ERROR! nPSF must be positive")
+		raise ValueError("ERROR! nPSF must be positive")
 
 	# Computing non-padded model size
 	nz_nopad = nz - 2*fat - zPadMinus - zPadPlus
 	nx_nopad = nx - 2*fat - xPadMinus - xPadPlus
 	PSF1_pos = np.linspace(zPadMinus+fat, zPadMinus+fat+nz_nopad, nPSF1).astype(np.int)
 	PSF2_pos = np.linspace(xPadMinus+fat, xPadMinus+fat+nx_nopad, nPSF2).astype(np.int)
-	if nPSF3 != 1:
-		PSF3_pos = np.linspace(0, nExt-1, nPSF3).astype(np.int)
-		zz,xx,ee = np.meshgrid(PSF1_pos,PSF2_pos,PSF3_pos)
-		model.getNdArray()[ee,xx,zz] = 1.0
-	else:
-		# Filling positions of the PSF with ones
-		zz,xx = np.meshgrid(PSF1_pos,PSF2_pos)
-		model.getNdArray()[0,xx,zz] = 1.0
-
+	PSF3_pos = np.linspace(0, nExt-1, nPSF3).astype(np.int)
+	zz,xx,ee = np.meshgrid(PSF1_pos,PSF2_pos,PSF3_pos,indexing='ij',sparse=True)
+	model.getNdArray()[ee,xx,zz] = 1.0
 	model.writeVec(PSFile)
